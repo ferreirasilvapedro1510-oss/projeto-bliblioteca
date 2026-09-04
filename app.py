367cc955af489b6397d601124b3867fdce316db5
Pedro <ferreira.silva.pedro1510@escola.pr.gov.br>
2026-09-04 08:41:08 -0300

    Cadastro de alunos

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, redirect
 import mysql.connector
 from config import DB_CONFIG
 
@@ -17,6 +17,7 @@ def index():
     <p>Projeto iniciado com Python, Flask e MySQL.</p>
     <ul>
         <li><a href="/alunos">Ver alunos cadastrados</a></li>
+        <li><a href="/alunos/novo">Cadastrar Alunos</a></li>
         <li><a href="/professores">Ver professores cadastrados</a></li>
         <li><a href="/bibliotecarios">Ver bibliotecários cadastrados</a></li>
         <li><a href="/livros">Ver livros cadastrados</a></li>
@@ -68,6 +69,54 @@ def listar_alunos():
     except Exception as erro:
         return f"Erro ao listar alunos: {erro}"
 
+@app.route("/alunos/novo")
+def formulario_aluno():
+    return """
+<h1>Cadastrar Alunos</h1>
+
+<form method="POST" action="/alunos/cadastrar">
+    <label>Nome: </label></br>
+    <input type="text" name="nome" required></br></br>
+
+    <label>Série: </label></br>
+    <input type="text" name="serie" required></br></br>
+
+    <label>Turma: </label></br>
+    <input type="text" name="turma" required></br></br>
+
+    <label>Telefone: </label></br>
+    <input type="text" name="telefone" required></br></br>
+
+    <button type="submite">Salvar</button>
+</form>
+<br>
+<a href="/alunos"> Voltar para a lista</a>
+"""
+
+@app.route("/alunos/cadastrar", methods=["POST"])
+def cadastrar_alunos():
+    nome = request.form["nome"]
+    serie = request.form["serie"]
+    turma = request.form["turma"]
+    telefone = request.form["telefone"]
+
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    sql = """
+    INSERT INTO aluno(nome, serie, turma, telefone)
+    values (%s,%s,%s,%s)
+    """
+
+    valores = (nome, serie, turma, telefone)
+
+    cursor.execute(sql, valores)
+    conexao.commit()
+
+    cursor.close()
+    conexao.close()
+
+    return redirect("/alunos")
 
 @app.route("/professores")
 def listar_professores():
